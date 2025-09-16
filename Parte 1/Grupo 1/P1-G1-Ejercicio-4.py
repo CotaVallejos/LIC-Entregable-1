985cf5daefe26427cb97c75fc04b51728e19e38f
CotaVallejos <constanza.vallejos.s@gmail.com>
2025-09-16 18:20:18 -0300

    Ejercicio 4 listo

---
+++ Parte 1/Grupo 1/P1-G1-Ejercicio-4.py
@@ -0,0 +1,25 @@
+# Parte 1: 20 Ejercicios Intermedios de Python
+
+## Grupo 1: Fundamentos y Lógica (Ejercicios 1-5)
+
+### 4. Adivina el Número: El programa genera un número aleatorio entre 1 y 100. El usuario debe adivinarlo y el programa le dará pistas ("más alto" o "más bajo").
+
+import random
+
+# Generar un número aleatorio entre 1 y 100
+numeroSecreto = random.randint(1, 100)
+
+# Inicializar la variable del usuario
+numeroUsuario = -1
+
+# Mientras el usuario no adivine
+while numeroUsuario != numeroSecreto:
+# Pedir al usuario que adivine el número
+    numeroUsuario = int(input("¡Adivina el número! Ingresa un número entre 1 y 100:"))
+
+    if numeroUsuario == numeroSecreto:
+        print(f"¡Felicitaciones, adivinaste! El número es {numeroSecreto}.")
+    elif numeroUsuario > numeroSecreto:
+        print("Más bajo")
+    else:
+        print("Más alto")
